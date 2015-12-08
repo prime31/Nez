fd844b0e5a9f9ccac5d1d44f181ab9e1c621fc23
Mike <mike@prime31.com>
2015-12-07 23:48:38 -0800

    lots of random additions

--- Nez-PCL/Math/Random.cs
+++ Nez-PCL/Math/Random.cs
@@ -9,21 +9,44 @@ namespace Nez
 		public static System.Random random = new System.Random();
 
 
+		/// <summary>
+		/// returns a random float between 0 and 1
+		/// </summary>
+		/// <returns>The float.</returns>
 		static public float nextFloat()
 		{
 			return (float)random.NextDouble();
 		}
 
 
+		/// <summary>
+		/// returns a random float between 0 and max
+		/// </summary>
+		/// <returns>The float.</returns>
+		/// <param name="max">Max.</param>
 		static public float nextFloat( float max )
 		{
 			return nextFloat() * max;
 		}
 
 
+		/// <summary>
+		/// returns a random float between 0 and 2 * PI
+		/// </summary>
+		/// <returns>The angle.</returns>
 		static public float nextAngle()
 		{
 			return nextFloat() * MathHelper.TwoPi;
+		}
+
+
+		/// <summary>
+		/// returns a random color
+		/// </summary>
+		/// <returns>The color.</returns>
+		public static Color nextColor()
+		{
+			return new Color( nextFloat(), nextFloat(), nextFloat() );
 		}
 
 
@@ -64,6 +87,17 @@ namespace Nez
 		{
 			return min + new Vector2( nextFloat( max.X - min.X ), nextFloat( max.Y - min.Y ) );
 		}
+
+
+		/// <summary>
+		/// returns a random float between -1 and 1
+		/// </summary>
+		/// <returns>The one to one.</returns>
+		static public float minusOneToOne()
+		{
+			return nextFloat( 2f ) - 1f;
+		}
+
 	}
 }
 
