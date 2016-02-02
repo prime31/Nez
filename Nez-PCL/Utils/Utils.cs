c35d8149201a5d55525264d325161d871b1e223d
Mike <mike@prime31.com>
2016-02-01 23:49:29 -0800

    added Utils class

---
+++ Nez-PCL/Utils/Utils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+
+namespace Nez
+{
+	/// <summary>
+	/// utility methods that don't yet have a proper home that makes sense
+	/// </summary>
+	public static class Utils
+	{
+		public static string randomString( int size = 38 )
+		{
+			var builder = new StringBuilder();
+
+			char ch;
+			for( int i = 0; i < size; i++ )
+			{
+				ch = Convert.ToChar( Convert.ToInt32( Math.Floor( 26 * Random.nextFloat() + 65 ) ) );
+				builder.Append( ch );
+			}
+
+			return builder.ToString();
+		}
+	}
+}
+
