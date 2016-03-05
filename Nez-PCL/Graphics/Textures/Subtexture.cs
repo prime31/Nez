a114d78cdd93eb39a0ee709de4097ca9a01274cf
Mike <mike@prime31.com>
2016-03-05 11:04:08 -0800

    more power for creating animations on the fly

--- Nez-PCL/Graphics/Textures/Subtexture.cs
+++ Nez-PCL/Graphics/Textures/Subtexture.cs
@@ -63,18 +63,29 @@ namespace Nez.Textures
 		/// <param name="texture">Texture.</param>
 		/// <param name="cellWidth">Cell width.</param>
 		/// <param name="cellHeight">Cell height.</param>
-		public static List<Subtexture> subtexturesFromAtlas( Texture2D texture, int cellWidth, int cellHeight )
+		/// <param name="cellOffset">the first cell to include while processing. 0 based indexing.</param>
+		/// <param name="maxCellsToInclude">Max cells to included.</param>
+		public static List<Subtexture> subtexturesFromAtlas( Texture2D texture, int cellWidth, int cellHeight, int cellOffset = 0, int maxCellsToInclude = int.MaxValue )
 		{
 			var subtextures = new List<Subtexture>();
 
 			var cols = texture.Width / cellWidth;
 			var rows = texture.Height / cellHeight;
+			var i = 0;
 
 			for( var y = 0; y < rows; y++ )
 			{
 				for( var x = 0; x < cols; x++ )
 				{
+					// skip everything before the first cellOffset
+					if( i++ < cellOffset )
+						continue;
+					
 					subtextures.Add( new Subtexture( texture, new Rectangle( x * cellWidth, y * cellHeight, cellWidth, cellHeight ) ) );
+
+					// once we hit the max number of cells to include bail out. were done.
+					if( subtextures.Count == maxCellsToInclude )
+						break;
 				}
 			}
 
