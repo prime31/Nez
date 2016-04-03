d40e4c87f61de8e65f89b47a65cfe19ff7efd70b
Mike <mike@prime31.com>
2016-04-03 00:05:05 -0700

    better circle to polygon collision detection

--- Nez-PCL/Physics/Shapes/Polygon.cs
+++ Nez-PCL/Physics/Shapes/Polygon.cs
@@ -71,6 +71,45 @@ namespace Nez.PhysicsShapes
 		}
 
 
+		/// <summary>
+		/// iterates all the edges of the polygon and gets the closest point on any edge to point. Returns via out the squared distance
+		/// to the closest point and the normal of the edge it is on.
+		/// </summary>
+		/// <returns>The closest point on polygon to point.</returns>
+		/// <param name="point">Point.</param>
+		/// <param name="distanceSquared">Distance squared.</param>
+		/// <param name="edgeNormal">Edge normal.</param>
+		public Vector2 getClosestPointOnPolygonToPoint( Vector2 point, out float distanceSquared, out Vector2 edgeNormal )
+		{
+			distanceSquared = float.MaxValue;
+			edgeNormal = Vector2.Zero;
+			var closestPoint = Vector2.Zero;
+			float tempDistanceSquared;
+			for( var i = 0; i < points.Length; i++ )
+			{
+				var j = i + 1;
+				if( j == points.Length )
+					j = 0;
+				
+				var closest = ShapeCollisions.closestPointOnLine( points[i], points[j], point );
+				Vector2.DistanceSquared( ref point, ref closest, out tempDistanceSquared );
+
+				if( tempDistanceSquared < distanceSquared )
+				{
+					distanceSquared = tempDistanceSquared;
+					closestPoint = closest;
+
+					// get the normal of the line
+					var line = points[j] - points[i];
+					edgeNormal.X = line.Y;
+					edgeNormal.Y = -line.X;
+				}
+			}
+
+			return closestPoint;
+		}
+
+
 		#region Shape abstract methods
 
 		public override bool overlaps( Shape other )
@@ -80,7 +119,14 @@ namespace Nez.PhysicsShapes
 				return ShapeCollisions.polygonToPolygon( this, other as Polygon, out result );
 
 			if( other is Circle )
-				ShapeCollisions.circleToPolygon( other as Circle, this, out result );
+			{
+				if( ShapeCollisions.circleToPolygon( other as Circle, this, out result ) )
+				{
+					result.invertResult();
+					return true;
+				}
+				return false;
+			}
 
 			throw new NotImplementedException( string.Format( "overlaps of Polygon to {0} are not supported", other ) );
 		}
