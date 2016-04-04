e5548a2c4a50036a87486bea3849bcc836050bf7
Mike <mike@prime31.com>
2016-04-04 08:37:19 -0700

    added the SpriteMime to mimic another sprite

---
+++ Nez-PCL/ECS/Components/Renderables/Sprites/SpriteMime.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Nez.Sprites;
+using Microsoft.Xna.Framework;
+
+
+namespace Nez.Sprites
+{
+	/// <summary>
+	/// this component will draw the same frame of of spriteToMime every frame. The only difference in rendering is that SpriteMime uses its own
+	/// localPosition and color. This allows you to use it for the purpose of shadows (by offsetting via localPosition) or silhouettes (with a
+	/// RenderState that has a stencil read).
+	/// </summary>
+	public class SpriteMime : RenderableComponent
+	{
+		public override float width { get { return _spriteToMime.width; } }
+
+		public override float height { get { return _spriteToMime.height; } }
+
+
+		Sprite _spriteToMime;
+
+
+		public SpriteMime( Sprite spriteToMime )
+		{
+			_spriteToMime = spriteToMime;
+		}
+
+
+		public override void render( Graphics graphics, Camera camera )
+		{
+			if( isVisibleFromCamera( camera ) )
+				graphics.spriteBatch.Draw( _spriteToMime.subtexture, entity.transform.position + _localPosition, _spriteToMime.subtexture.sourceRect, color, entity.transform.rotation, _spriteToMime.origin, entity.transform.scale, _spriteToMime.spriteEffects, _layerDepth );
+		}
+	}
+}
+
