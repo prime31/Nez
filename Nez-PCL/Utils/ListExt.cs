881b754e60abe034ce865648c8edfe5114a36ed0
Mike <mike@prime31.com>
2016-05-02 12:38:34 -0700

    shuffle extension for Lists

---
+++ Nez-PCL/Utils/ListExt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Nez
+{
+	public static class ListExt
+	{
+		public static void shuffle<T>( this IList<T> list )
+		{
+			var n = list.Count;
+			while( n > 1 )
+			{
+				n--;
+				int k = Nez.Random.range( 0, n + 1 );
+				T value = list[k];
+				list[k] = list[n];
+				list[n] = value;
+			}  
+		}
+	}
+}
+
