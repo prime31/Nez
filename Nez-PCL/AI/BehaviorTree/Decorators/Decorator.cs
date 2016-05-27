5928ffb0d0f43ac33d6c78560660c9cb9ca77d5a
Mike <mike@prime31.com>
2016-05-27 13:58:15 -0700

    fixes #37

--- Nez-PCL/AI/BehaviorTree/Decorators/Decorator.cs
+++ Nez-PCL/AI/BehaviorTree/Decorators/Decorator.cs
@@ -6,6 +6,13 @@ namespace Nez.AI.BehaviorTrees
 	public abstract class Decorator<T> : Behavior<T>
 	{
 		public Behavior<T> child;
+
+
+		public override void invalidate()
+		{
+			base.invalidate();
+			child.invalidate();
+		}
 	}
 }
 
