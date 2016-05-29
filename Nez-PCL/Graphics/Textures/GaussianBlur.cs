faedb8e53dbfaeeb677a6996c4dea7a74588cc2c
Mike <mike@prime31.com>
2016-05-28 22:25:16 -0700

    texture utils! gaussian blur, and normal maps from height maps

---
+++ Nez-PCL/Graphics/Textures/GaussianBlur.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace Nez.Textures
+{
+	public class GaussianBlur
+	{
+		/// <summary>
+		/// creates a new texture that is a gaussian blurred version of the original
+		/// </summary>
+		/// <returns>The blurred texture.</returns>
+		/// <param name="image">Image.</param>
+		/// <param name="deviation">Deviation.</param>
+		public static Texture2D createdBlurredTexture( Texture2D image, double deviation = 1 )
+		{
+			var resultTex = new Texture2D( Core.graphicsDevice, image.Width, image.Height, false, SurfaceFormat.Color );
+			var matrix = new double[image.Width, image.Height];
+
+			var destData = new Color[image.Width * image.Height];
+			var srcData = new Color[image.Width * image.Height];
+			image.GetData<Color>( srcData );
+
+
+			for( var i = 0; i < image.Width; i++ )
+			{
+				for( var j = 0; j < image.Height; j++ )
+					matrix[i, j] = srcData[i + j * image.Width].grayscale().R;
+			}
+
+			matrix = GaussianBlur.gaussianConvolution( matrix, deviation );
+			for( var i = 0; i < image.Width; i++ )
+			{
+				for( var j = 0; j < image.Height; j++ )
+				{
+					var val = (int)Math.Min( 255, matrix[i, j] );
+					destData[i + j * image.Width] = new Color( val, val, val, 255 );
+				}
+			}
+
+			resultTex.SetData( destData );
+
+			return resultTex;
+		}
+
+
+		static double[,] calculate1DSampleKernel( double deviation, int size )
+		{
+			double[,] ret = new double[size, 1];
+			double sum = 0;
+			//int half = size / 2; // originally used this but I dont think it is correct since it creates incorrect half values
+			double half = ( size - 1 ) / 2;
+			for( var i = 0; i < size; i++ )
+			{
+				ret[i, 0] = 1 / ( Math.Sqrt( 2 * Math.PI ) * deviation ) * Math.Exp( -( i - half ) * ( i - half ) / ( 2 * deviation * deviation ) );
+				sum += ret[i, 0];
+			}
+			return ret;
+		}
+
+
+		static double[,] calculate1DSampleKernel( double deviation )
+		{
+			var size = (int)Math.Ceiling( deviation * 3 ) * 2 + 1;
+			return calculate1DSampleKernel( deviation, size );
+		}
+
+
+		static double[,] calculateNormalized1DSampleKernel( double deviation )
+		{
+			return normalizeMatrix( calculate1DSampleKernel( deviation ) );
+		}
+
+
+		static double[,] normalizeMatrix( double[,] matrix )
+		{
+			var ret = new double[matrix.GetLength( 0 ), matrix.GetLength( 1 )];
+			double sum = 0;
+			for( var i = 0; i < ret.GetLength( 0 ); i++ )
+			{
+				for( var j = 0; j < ret.GetLength( 1 ); j++ )
+					sum += matrix[i, j];
+			}
+
+			if( sum != 0 )
+			{
+				for( var i = 0; i < ret.GetLength( 0 ); i++ )
+				{
+					for( var j = 0; j < ret.GetLength( 1 ); j++ )
+						ret[i, j] = matrix[i, j] / sum;
+				}
+			}
+			return ret;
+		}
+
+
+		static double[,] gaussianConvolution( double[,] matrix, double deviation )
+		{
+			var kernel = calculateNormalized1DSampleKernel( deviation );
+			var res1 = new double[matrix.GetLength( 0 ), matrix.GetLength( 1 )];
+			var res2 = new double[matrix.GetLength( 0 ), matrix.GetLength( 1 )];
+
+			// x-direction
+			for( var i = 0; i < matrix.GetLength( 0 ); i++ )
+			{
+				for( var j = 0; j < matrix.GetLength( 1 ); j++ )
+					res1[i, j] = processPoint( matrix, i, j, kernel, 0 );
+			}
+
+			// y-direction
+			for( var i = 0; i < matrix.GetLength( 0 ); i++ )
+			{
+				for( var j = 0; j < matrix.GetLength( 1 ); j++ )
+					res2[i, j] = processPoint( res1, i, j, kernel, 1 );
+			}
+			return res2;
+		}
+
+
+		static double processPoint( double[,] matrix, int x, int y, double[,] kernel, int direction )
+		{
+			double res = 0;
+			var half = kernel.GetLength( 0 ) / 2;
+			for( var i = 0; i < kernel.GetLength( 0 ); i++ )
+			{
+				var cox = direction == 0 ? x + i - half : x;
+				var coy = direction == 1 ? y + i - half : y;
+				if( cox >= 0 && cox < matrix.GetLength( 0 ) && coy >= 0 && coy < matrix.GetLength( 1 ) )
+					res += matrix[cox, coy] * kernel[i, 0];
+			}
+			return res;
+		}
+
+	}
+}
+
---
+++ Nez-PCL/Graphics/Textures/TextureUtils.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+
+namespace Nez.Textures
+{
+	public static class TextureUtils
+	{
+		public static Texture2D createdFlatHeightmap( Texture2D image, Color opaqueColor, Color transparentColor )
+		{
+			var resultTex = new Texture2D( Core.graphicsDevice, image.Width, image.Height, false, SurfaceFormat.Color );
+
+			var destData = new Color[image.Width * image.Height];
+			var srcData = new Color[image.Width * image.Height];
+			image.GetData<Color>( srcData );
+
+			for( var i = 1; i < image.Width - 1; i++ )
+			{
+				for( var j = 1; j < image.Height - 1; j++ )
+				{
+					var pixel = srcData[i + j * image.Width];
+
+					if( pixel.A == 0 )
+						destData[i + j * image.Width] = transparentColor;
+					else
+						destData[i + j * image.Width] = opaqueColor;
+				}
+			}
+
+			resultTex.SetData( destData );
+
+			return resultTex;
+		}
+
+		
+		/// <summary>
+		/// creates a new texture that is a gaussian blurred version of the original
+		/// </summary>
+		/// <returns>The blurred texture.</returns>
+		/// <param name="image">Image.</param>
+		/// <param name="deviation">Deviation.</param>
+		public static Texture2D createdBlurredTexture( Texture2D image, double deviation = 1 )
+		{
+			return GaussianBlur.createdBlurredTexture( image, deviation );
+		}
+
+
+		/// <summary>
+		/// generates a normal map from a height map calculating it with a sobel filter
+		/// </summary>
+		/// <returns>The sobel filter.</returns>
+		/// <param name="image">Image.</param>
+		/// <param name="normalStrength">Normal strength.</param>
+		public static Texture2D createNormalMapWithSobelFilter( Texture2D image, float normalStrength = 1f, bool invertX = false, bool invertY = false )
+		{
+			// x/r is inverted here due to the sobel kernel used so we use the opposite to keep it the same as createNormalMap
+			var invertR = invertX ? 1f : -1f;
+			var invertG = invertY ? -1f : 1f;
+			var resultTex = new Texture2D( Core.graphicsDevice, image.Width, image.Height, false, SurfaceFormat.Color );
+
+			var destData = new Color[image.Width * image.Height];
+			var srcData = new Color[image.Width * image.Height];
+			image.GetData<Color>( srcData );
+
+			for( var i = 1; i < image.Width - 1; i++ )
+			{
+				for( var j = 1; j < image.Height - 1; j++ )
+				{
+					var cr = srcData[i + 1 + j * image.Width].grayscale().B / 255f;
+					var cl = srcData[i - 1 + j * image.Width].grayscale().B / 255f;
+					var cu = srcData[i + ( j - 1 ) * image.Width].grayscale().B / 255f;
+					var cd = srcData[i + ( j + 1 ) * image.Width].grayscale().B / 255f;
+					var cld = srcData[i - 1 + ( j + 1 ) * image.Width].grayscale().B / 255f;
+					var clu = srcData[i - 1 + ( j - 1 ) * image.Width].grayscale().B / 255f;
+					var crd = srcData[i + 1 + ( j + 1 ) * image.Width].grayscale().B / 255f;
+					var cru = srcData[i + 1 + ( j - 1 ) * image.Width].grayscale().B / 255f;
+
+					// Compute dx using Sobel:
+					//           -1 0 1 
+					//           -2 0 2
+					//           -1 0 1
+					var dX = cru + 2 * cr + crd - clu - 2 * cl - cld;
+
+					// Compute dy using Sobel:
+					//           -1 -2 -1 
+					//            0  0  0
+					//            1  2  1
+					var dY = cld + 2 * cd + crd - clu - 2 * cu - cru;
+
+					var normal = Vector3.Normalize( new Vector3( dX * invertR, dY * invertG, 1f / normalStrength ) );
+					normal = normal * 0.5f + new Vector3( 0.5f );
+					destData[i + j * image.Width] = new Color( normal.X, normal.Y, normal.Z, 1f );
+				}
+			}
+
+			resultTex.SetData( destData );
+
+			return resultTex;
+		}
+
+
+		/// <summary>
+		/// generates a normal map from a height map calculating it with just the 4 surrounding pixels
+		/// </summary>
+		/// <returns>The normal map.</returns>
+		/// <param name="image">Image.</param>
+		/// <param name="bias">Bias.</param>
+		/// <param name="invertRed">If set to <c>true</c> invert red.</param>
+		/// <param name="invertGreen">If set to <c>true</c> invert green.</param>
+		public static Texture2D createNormalMap( Texture2D image, float bias = 50f, bool invertX = false, bool invertY = false )
+		{
+			var invertR = invertX ? -1f : 1f;
+			var invertG = invertY ? -1f : 1f;
+			var resultTex = new Texture2D( Core.graphicsDevice, image.Width, image.Height, false, SurfaceFormat.Color );
+
+			var destData = new Color[image.Width * image.Height];
+			var srcData = new Color[image.Width * image.Height];
+			image.GetData<Color>( srcData );
+
+			for( var i = 1; i < image.Width - 1; i++ )
+			{
+				for( var j = 1; j < image.Height - 1; j++ )
+				{
+					var d0 = srcData[i + j * image.Width].grayscale().B / 255f;
+					var d1 = srcData[i + 1 + j * image.Width].grayscale().B / 255f;
+					var d2 = srcData[i - 1 + j * image.Width].grayscale().B / 255f;
+					var d3 = srcData[i + ( j - 1 ) * image.Width].grayscale().B / 255f;
+					var d4 = srcData[i + ( j + 1 ) * image.Width].grayscale().B / 255f;
+
+					var dx = ( ( d2 - d0 ) + ( d0 - d1 ) ) * 0.5f;
+					var dy = ( ( d4 - d0 ) + ( d0 - d3 ) ) * 0.5f;
+
+					var normal = new Vector3( dx * invertR, dy * invertG, 1.0f - ( ( bias - 0.1f ) / 100.0f ) );
+					normal.Normalize();
+					normal = normal * 0.5f + new Vector3( 0.5f );
+					destData[i + j * image.Width] = new Color( normal.X, normal.Y, normal.Z, 1f );
+				}
+			}
+
+			resultTex.SetData( destData );
+
+			return resultTex;
+		}
+
+	}
+}
+
