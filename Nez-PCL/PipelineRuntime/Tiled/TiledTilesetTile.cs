11d2689b37a2b9375250f39ca940bf56068ef7f6
Mike <mike@prime31.com>
2016-06-08 14:19:58 -0700

    added 3 "magic" properties that will be auto-parsed and cached

--- Nez-PCL/PipelineRuntime/Tiled/TiledTilesetTile.cs
+++ Nez-PCL/PipelineRuntime/Tiled/TiledTilesetTile.cs
@@ -14,6 +14,53 @@ namespace Nez.Tiled
 		public List<TiledTileAnimationFrame> animationFrames;
 		public Dictionary<string,string> properties = new Dictionary<string,string>();
 
+		/// <summary>
+		/// returns the value of an "isDestructable" property if present in the properties dictionary
+		/// </summary>
+		/// <value><c>true</c> if is destructable; otherwise, <c>false</c>.</value>
+		public bool isDestructable
+		{
+			get
+			{
+				if( !_isDestructable.HasValue && properties.ContainsKey( "isDestructable" ) )
+					_isDestructable = bool.Parse( properties["isDestructable"] );
+				return _isDestructable.Value;
+			}
+		}
+
+		/// <summary>
+		/// returns the value of an "slopeTopLeft" property if present in the properties dictionary
+		/// </summary>
+		/// <value>The slope top left.</value>
+		public int slopeTopLeft
+		{
+			get
+			{
+				if( !_slopeTopLeft.HasValue && properties.ContainsKey( "slopeTopLeft" ) )
+					_slopeTopLeft = int.Parse( properties["slopeTopLeft"] );
+				return _slopeTopLeft.Value;
+			}
+		}
+
+		/// <summary>
+		/// returns the value of an "slopeTopRight" property if present in the properties dictionary
+		/// </summary>
+		/// <value>The slope top right.</value>
+		public int slopeTopRight
+		{
+			get
+			{
+				if( !_slopeTopRight.HasValue && properties.ContainsKey( "slopeTopRight" ) )
+					_slopeTopRight = int.Parse( properties["slopeTopRight"] );
+				return _slopeTopRight.Value;
+			}
+		}
+
+		bool? _isDestructable;
+		int? _slopeTopLeft;
+		int? _slopeTopRight;
+
+
 
 		public TiledTilesetTile( int id )
 		{
