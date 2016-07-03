5d41710f638159ce0ac87750ce8d3eb342d30aa3
Mike <mike@prime31.com>
2016-07-03 14:53:04 -0700

    BREAKING CHANGE to Texture Packer importer!
    Make TexturePacker importer region names be the full "filename" field from the JSON file minus the file extension

--- Nez.PipelineImporter/TexturePacker/TexturePackerProcessor.cs
+++ Nez.PipelineImporter/TexturePacker/TexturePackerProcessor.cs
@@ -7,8 +7,12 @@ namespace Nez.TexturePackerImporter
 	[ContentProcessor( DisplayName = "TexturePacker Processor" )]
 	public class TexturePackerProcessor : ContentProcessor<TexturePackerFile, TexturePackerFile>
 	{
+		public static ContentBuildLogger logger;
+
+
 		public override TexturePackerFile Process( TexturePackerFile input, ContentProcessorContext context )
 		{
+			logger = context.Logger;
 			return input;
 		}
 	}
