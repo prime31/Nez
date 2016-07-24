875f314864452ed7dfa7e37da6aa79a5c607a630
Mike <mike@prime31.com>
2016-07-24 16:33:57 -0700

    break out trigger handling from Mover into its own helper class

--- Nez-PCL/ECS/Components/Physics/Mover.cs
+++ Nez-PCL/ECS/Components/Physics/Mover.cs
@@ -13,17 +13,13 @@ namespace Nez
 	/// </summary>
 	public class Mover : Component
 	{
-		/// <summary>
-		/// stores all the active intersection pairs that occured in the current frame
-		/// </summary>
-		HashSet<Pair<Collider>> _activeTriggerIntersections = new HashSet<Pair<Collider>>();
+		ColliderTriggerHelper _triggerHelper;
 
-		/// <summary>
-		/// stores the previous frames intersection pairs so that we can detect exits after moving this frame
-		/// </summary>
-		HashSet<Pair<Collider>> _previousTriggerIntersections = new HashSet<Pair<Collider>>();
 
-		List<ITriggerListener> _tempTriggerList = new List<ITriggerListener>();
+		public override void onAddedToEntity()
+		{
+			_triggerHelper = new ColliderTriggerHelper( entity );
+		}
 
 
 		/// <summary>
@@ -80,87 +76,13 @@ namespace Nez
 
 			// 3. do an overlap check of all entity.colliders that are triggers with all broadphase colliders, triggers or not.
 			//    Any overlaps result in trigger events.
-			for( var i = 0; i < entity.colliders.Count; i++ )
-			{
-				var collider = entity.colliders[i];
-
-				// fetch anything that we might collide with us at our new position
-				var neighbors = Physics.boxcastBroadphase( collider.bounds, collider.collidesWithLayers );
-				foreach( var neighbor in neighbors )
-				{
-					// we need at least one of the colliders to be a trigger
-					if( !collider.isTrigger && !neighbor.isTrigger )
-						continue;
-
-					if( collider.overlaps( neighbor ) )
-					{
-						var pair = new Pair<Collider>( collider, neighbor );
-
-						// if we already have this pair in one of our sets (the previous or current trigger intersections) dont call the enter event
-						var shouldReportTriggerEvent = !_activeTriggerIntersections.Contains( pair ) && !_previousTriggerIntersections.Contains( pair );
-						if( shouldReportTriggerEvent )
-							notifyTriggerListeners( pair, true );
-
-						_activeTriggerIntersections.Add( pair );
-					} // overlaps
-				} // end foreach
-			}
+			_triggerHelper.update();
 
 			// let Physics know about our new position
 			entity.colliders.registerAllCollidersWithPhysicsSystem();
 
-			checkForExitedColliders();
-
 			return collisionResult.collider != null;
 		}
-
-
-		void checkForExitedColliders()
-		{
-			// remove all the triggers that we did interact with this frame leaving us with the ones we exited
-			_previousTriggerIntersections.ExceptWith( _activeTriggerIntersections );
-
-			foreach( var pair in _previousTriggerIntersections )
-				notifyTriggerListeners( pair, false );
-
-			// clear out the previous set cause we are done with it for now
-			_previousTriggerIntersections.Clear();
-
-			// add in all the currently active triggers
-			_previousTriggerIntersections.UnionWith( _activeTriggerIntersections );
-
-			// clear out the active set in preparation for the next frame
-			_activeTriggerIntersections.Clear();
-		}
-
-
-		void notifyTriggerListeners( Pair<Collider> collisionPair, bool isEntering )
-		{
-			// call the onTriggerEnter method for any relevant components
-			collisionPair.first.entity.getComponents( _tempTriggerList );
-			for( var i = 0; i < _tempTriggerList.Count; i++ )
-			{
-				if( isEntering )
-					_tempTriggerList[i].onTriggerEnter( collisionPair.second, collisionPair.first );
-				else
-					_tempTriggerList[i].onTriggerExit( collisionPair.second, collisionPair.first );
-			}
-
-			_tempTriggerList.Clear();
-
-			// also call it for the collider we moved onto
-			collisionPair.second.entity.getComponents( _tempTriggerList );
-			for( var i = 0; i < _tempTriggerList.Count; i++ )
-			{
-				if( isEntering )
-					_tempTriggerList[i].onTriggerEnter( collisionPair.first, collisionPair.second );
-				else
-					_tempTriggerList[i].onTriggerExit( collisionPair.first, collisionPair.second );
-			}
-
-			_tempTriggerList.Clear();
-		}
-	
 	}
 }
 
