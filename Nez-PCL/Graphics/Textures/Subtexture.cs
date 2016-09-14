0d486b44d33a1992dfd41915da36fcf00403d5c8
Mike <mike@prime31.com>
2016-09-14 14:34:26 -0700

    pretty big, possibly breaking changes here! Subtexture gained an origin field. Whenever a Sprite.subtexture is set the origin will also be set to match Subtexture.origin

--- Nez-PCL/Graphics/Textures/Subtexture.cs
+++ Nez-PCL/Graphics/Textures/Subtexture.cs
@@ -25,14 +25,24 @@ namespace Nez.Textures
 		/// </summary>
 		/// <value>The center.</value>
 		public readonly Vector2 center;
-		
 
-		public Subtexture( Texture2D texture, Rectangle sourceRect )
+		/// <summary>
+		/// the origin that a RenderableComponent should use when using this Subtexture. Defaults to the center.
+		/// </summary>
+		public Vector2 origin;
+
+
+		public Subtexture( Texture2D texture, Rectangle sourceRect, Vector2 origin )
 		{
-			this.texture2D = texture;
+			texture2D = texture;
 			this.sourceRect = sourceRect;
 			center = new Vector2( sourceRect.Width * 0.5f, sourceRect.Height * 0.5f );
+			this.origin = origin;
 		}
+		
+
+		public Subtexture( Texture2D texture, Rectangle sourceRect ) : this( texture, sourceRect, sourceRect.getHalfSize() )
+		{}
 
 
 		public Subtexture( Texture2D texture ) : this( texture, new Rectangle( 0, 0, texture.Width, texture.Height ) )
@@ -88,6 +98,18 @@ namespace Nez.Textures
 			destArray[6] = new Rectangle( renderRect.X, bottomY, marginLeft, marginBottom ); // bottom-left
 			destArray[7] = new Rectangle( leftX, bottomY, stretchedCenterWidth, marginBottom ); // bottom-center
 			destArray[8] = new Rectangle( rightX, bottomY, marginRight, marginBottom ); // bottom-right
+		}
+
+
+		/// <summary>
+		/// clones the Subtexture
+		/// </summary>
+		public Subtexture clone()
+		{
+			return new Subtexture( texture2D, sourceRect )
+			{
+				origin = origin
+			};
 		}
 
 
