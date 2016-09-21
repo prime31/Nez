95bd0e07850d99072e096e2b3a2bb8b3073ab19c
Mike Desaro <mike@prime31.com>
2016-09-21 13:40:23 -0700

    moving along with the LineRenderer. it now works. next up optimization and better api

--- Nez-PCL/ECS/Components/Renderables/LineRenderer.cs
+++ Nez-PCL/ECS/Components/Renderables/LineRenderer.cs
@@ -1,13 +1,13 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
 using System.Collections.Generic;
+using Nez.PhysicsShapes;
+using System.Runtime.CompilerServices;
 
 
-// TODO:	move isCCW to Vector2Ext
+// TODO:
 //			cache distances
-//			remove List for triangle indices
-//			make points Vector2
-//			?? allow width per point ??
+//			?? allow width/Color per point ??
 
 namespace Nez
 {
@@ -41,29 +41,34 @@ namespace Nez
 		/// </summary>
 		int _pointCount = 0;
 
-		Vector3[] _points;
+		Vector2[] _points;
 		VertexPositionColorTexture[] _vertices;
 		short[] _triangleIndices;
 		BasicEffect _basicEffect;
 		bool _areVertsDirty = true;
+
+		// state
+		Segment _firstSegment = new Segment();
+		Segment _secondSegment = new Segment();
+		List<short> _indices = new List<short>();
 
 
 		#region configuration
 
 		public LineRenderer setPointCount( int pointCount )
 		{
+			Assert.isTrue( pointCount > 1, "A line must have at least two points" );
 			_pointCount = pointCount;
 
 			// ensure we have room for all the positions
 			if( _points == null || _points.Length != pointCount )
 			{
-				_points = new Vector3[pointCount];
+				_points = new Vector2[pointCount];
 				_vertices = null;
 			}
 
 			// we need 5 verts for each point except the last point which needs no verts
-			if( _pointCount > 0 )
-				_vertices = new VertexPositionColorTexture[( pointCount - 1 ) * 5];
+			_vertices = new VertexPositionColorTexture[calculateTotalVerts()];
 			_areVertsDirty = true;
 
 			return this;
@@ -90,7 +95,7 @@ namespace Nez
 		}
 
 
-		public LineRenderer setPoint( int index, Vector3 point )
+		public LineRenderer setPoint( int index, Vector2 point )
 		{
 			Assert.isNotNull( _points, "points array is null. Did you call setPositionCount first?" );
 
@@ -100,7 +105,7 @@ namespace Nez
 		}
 
 
-		public LineRenderer setPoints( Vector3[] points )
+		public LineRenderer setPoints( Vector2[] points )
 		{
 			if( points.Length > _pointCount )
 				setPointCount( points.Length );
@@ -114,12 +119,34 @@ namespace Nez
 		#endregion
 
 
+		/// <summary>
+		/// Calculates the total verts
+		/// </summary>
+		/// <returns>The total verts.</returns>
+		int calculateTotalVerts()
+		{
+			var mid = ( _pointCount - 2 ) * 6;
+			var caps = 2 * 5;
+
+			return mid + caps;
+		}
+
+
+		public static float angle( Vector2 from, Vector2 to )
+		{
+			Vector2Ext.normalize( ref from );
+			Vector2Ext.normalize( ref to );
+			return Mathf.acos( Mathf.clamp( Vector2.Dot( from, to ), -1f, 1f ) ) * Mathf.rad2Deg;
+		}
+
+
 		void calculateVertices()
 		{
 			if( !_areVertsDirty || _pointCount == 0 )
 				return;
 
 			_areVertsDirty = false;
+			_indices.Clear();
 
 			var maxX = float.MinValue;
 			var minX = float.MaxValue;
@@ -129,24 +156,48 @@ namespace Nez
 			// calculate line length first
 			var lineLength = 0f;
 			for( var i = 0; i < _pointCount - 1; i++ )
-				lineLength += Vector3.Distance( _points[i], _points[i + 1] );
+			{
+				var ang = angle( _points[i], _points[i + 1] );
+				lineLength += Vector2.Distance( _points[i], _points[i + 1] );
+			}
 
-
-			var firstSegment = new Segment();
-			var secondSegment = new Segment();
-
-			var indices = new List<short>();
 			var distanceSoFar = 0f;
+			var fusedPoint = Vector2.Zero;
+			var vertIndex = 0;
+
+			// special case: single segment
+			if( _pointCount == 2 )
+			{
+				_firstSegment.setPoints( _points[0], _points[1], _startWidth, _endWidth );
+				addSingleSegmentLine( _firstSegment );
+				_triangleIndices = _indices.ToArray();
+
+				// update min/max for any visible verts
+				maxX = Mathf.maxOf( maxX, _firstSegment.tl.X, _firstSegment.tr.X, _firstSegment.br.X, _firstSegment.bl.X );
+				minX = Mathf.minOf( minX, _firstSegment.tl.X, _firstSegment.tr.X, _firstSegment.br.X, _firstSegment.bl.X );
+				maxY = Mathf.maxOf( maxY, _firstSegment.tl.Y, _firstSegment.tr.Y, _firstSegment.br.Y, _firstSegment.bl.Y );
+				minY = Mathf.minOf( minY, _firstSegment.tl.Y, _firstSegment.tr.Y, _firstSegment.br.Y, _firstSegment.bl.Y );
+
+				_bounds.x = minX;
+				_bounds.y = minY;
+				_bounds.width = maxX - minX;
+				_bounds.height = maxY - minY;
+				return;
+			}
+
+
 			for( var i = 0; i < _pointCount - 1; i++ )
 			{
 				var firstPoint = _points[i];
 				var secondPoint = _points[i + 1];
-				var vertIndex = i * 5;
+				Vector2? thirdPoint = null;
+				if( _pointCount > i + 2 )
+					thirdPoint = _points[i + 2];
 
 				// we need the distance along the line of both the first and second points. distanceSoFar will always be for the furthest point
 				// which is the previous point before adding the current segment distance.
 				var firstPointDistance = distanceSoFar;
-				distanceSoFar += Vector3.Distance( firstPoint, secondPoint );
+				distanceSoFar += Vector2.Distance( firstPoint, secondPoint );
 
 				var firstPointRatio = firstPointDistance / lineLength;
 				var secondPointRatio = distanceSoFar / lineLength;
@@ -161,89 +212,37 @@ namespace Nez
 
 				if( i == 0 )
 				{
-					firstSegment.setPoints( _points[i], _points[i + 1], firstPointWidth, secondPointWidth );
+					_firstSegment.setPoints( firstPoint, secondPoint, firstPointWidth, secondPointWidth );
+					_secondSegment.setPoints( secondPoint, thirdPoint.Value, firstPointWidth, secondPointWidth );
 				}
 				else
 				{
-					Utils.swap( ref firstSegment, ref secondSegment );
-					secondSegment.setPoints( _points[i], _points[i + 1], firstPointWidth, secondPointWidth );
-					secondSegment.processVerts( firstSegment );
+					Utils.swap( ref _firstSegment, ref _secondSegment );
+					if( thirdPoint.HasValue )
+						_secondSegment.setPoints( secondPoint, thirdPoint.Value, firstPointWidth, secondPointWidth );
 				}
 
-
-				// rotate 90 degrees before calculating and cache cos/sin
-				var radians = Mathf.atan2( secondPoint.Y - firstPoint.Y, secondPoint.X - firstPoint.X );
-				radians += MathHelper.PiOver2;
-				var halfCos = Mathf.cos( radians ) * 0.5f;
-				var halfSin = Mathf.sin( radians ) * 0.5f;
-
-				var tl = firstPoint - new Vector3( firstPointWidth * halfCos, firstPointWidth * halfSin, 0 );
-				var tr = secondPoint - new Vector3( secondPointWidth * halfCos, secondPointWidth * halfSin, 0 );
-				var br = secondPoint + new Vector3( secondPointWidth * halfCos, secondPointWidth * halfSin, 0 );
-				var bl = firstPoint + new Vector3( firstPointWidth * halfCos, firstPointWidth * halfSin, 0 );
-
-
-				indices.Add( (short)vertIndex );
-				indices.Add( (short)( vertIndex + 1 ) );
-				indices.Add( (short)( vertIndex + 2 ) );
-
-				indices.Add( (short)vertIndex );
-				indices.Add( (short)( vertIndex + 2 ) );
-				indices.Add( (short)( vertIndex + 3 ) );
-
-				// tl
-				_vertices[vertIndex].Position = tl;
-				_vertices[vertIndex].TextureCoordinate = new Vector2( 0, 1 );
-				_vertices[vertIndex++].Color = firstPointColor;
-
-				// tr
-				_vertices[vertIndex].Position = tr;
-				_vertices[vertIndex].TextureCoordinate = new Vector2( 1, 1 );
-				_vertices[vertIndex++].Color = secondPointColor;
-
-				// br
-				_vertices[vertIndex].Position = br;
-				_vertices[vertIndex].TextureCoordinate = new Vector2( 1, 0 );
-				_vertices[vertIndex++].Color = secondPointColor;
-
-				// bl
-				_vertices[vertIndex].Position = bl;
-				_vertices[vertIndex].TextureCoordinate = new Vector2( 0, 0 );
-				_vertices[vertIndex++].Color = firstPointColor;
-
-				// first point
-				_vertices[vertIndex].Position = firstPoint;
-				_vertices[vertIndex].TextureCoordinate = new Vector2( 0, 0.5f );
-				_vertices[vertIndex++].Color = firstPointColor;
-
-				// for all segments except the first we need to clean up the joint/elbow by adding a new vert and triangle to fill the gap
-				if( i != 0 )
+				// dont recalculate the fusedPoint for the last segment since there will be no third point to work with
+				if( thirdPoint.HasValue )
 				{
-					// we need to fill the bottom gap when we have a counter-clockwise angle or the top for clockwise
-					var prevPoint = _points[i - 1];
-
-					if( isCCW( prevPoint, firstPoint, secondPoint ) )
-					{
-						indices.Add( (short)( vertIndex - 1 ) );
-						indices.Add( (short)( vertIndex - 9 ) ); // tr
-						indices.Add( (short)( vertIndex - 5 ) ); // tl
-					}
-					else
-					{
-						indices.Add( (short)( vertIndex - 1 ) );
-						indices.Add( (short)( vertIndex - 2 ) ); // bl
-						indices.Add( (short)( vertIndex - 8 ) ); // br
-					}
+					var shouldFuseBottom = Vector2Ext.isTriangleCCW( firstPoint, secondPoint, thirdPoint.Value );
+					_secondSegment.setFusedData( shouldFuseBottom, _firstSegment );
 				}
+
+				// special care needs to be take with the first segment since it has a different vert count
+				if( i == 0 )
+					addFirstSegment( _firstSegment, _secondSegment, ref vertIndex );
+				else
+					addSegment( _firstSegment, ref vertIndex );
 
 				// update min/max for any visible verts
-				maxX = Mathf.maxOf( maxX, tl.X, tr.X, br.X, bl.X );
-				minX = Mathf.minOf( minX, tl.X, tr.X, br.X, bl.X );
-				maxY = Mathf.maxOf( maxY, tl.Y, tr.Y, br.Y, bl.Y );
-				minY = Mathf.minOf( minY, tl.Y, tr.Y, br.Y, bl.Y );
+				maxX = Mathf.maxOf( maxX, _firstSegment.tl.X, _firstSegment.tr.X, _firstSegment.br.X, _firstSegment.bl.X );
+				minX = Mathf.minOf( minX, _firstSegment.tl.X, _firstSegment.tr.X, _firstSegment.br.X, _firstSegment.bl.X );
+				maxY = Mathf.maxOf( maxY, _firstSegment.tl.Y, _firstSegment.tr.Y, _firstSegment.br.Y, _firstSegment.bl.Y );
+				minY = Mathf.minOf( minY, _firstSegment.tl.Y, _firstSegment.tr.Y, _firstSegment.br.Y, _firstSegment.bl.Y );
 			}
 
-			_triangleIndices = indices.ToArray();
+			_triangleIndices = _indices.ToArray();
 
 			_bounds.x = minX;
 			_bounds.y = minY;
@@ -252,9 +251,120 @@ namespace Nez
 		}
 
 
-		bool isCCW( Vector3 p0, Vector3 p1, Vector3 p2 )
+		[MethodImpl( MethodImplOptions.AggressiveInlining )]
+		void addSingleSegmentLine( Segment segment )
 		{
-			return Triangulator.isTriangleCCW( p0.toVector2(), p1.toVector2(), p2.toVector2() );
+			_indices.Add( 0 );
+			_indices.Add( 1 );
+			_indices.Add( 2 );
+
+			_indices.Add( 0 );
+			_indices.Add( 2 );
+			_indices.Add( 3 );
+
+			addVert( 0, segment.tl, new Vector2( 0, 1 ), _startColor );
+			addVert( 1, segment.tr, new Vector2( 1, 1 ), _endColor );
+			addVert( 2, segment.br, new Vector2( 1, 0 ), _endColor );
+			addVert( 3, segment.bl, new Vector2( 0, 0 ), _startColor );
+		}
+
+
+		[MethodImpl( MethodImplOptions.AggressiveInlining )]
+		void addFirstSegment( Segment segment, Segment nextSegment, ref int vertIndex )
+		{
+			_indices.Add( 0 );
+			_indices.Add( 1 );
+			_indices.Add( 4 );
+
+			_indices.Add( 1 );
+			_indices.Add( 2 );
+			_indices.Add( 4 );
+
+			_indices.Add( 2 );
+			_indices.Add( 3 );
+			_indices.Add( 4 );
+
+			addVert( vertIndex++, segment.tl, new Vector2( 0, 1 ), _startColor );
+
+			if( nextSegment.shouldFuseBottom )
+			{
+				addVert( vertIndex++, segment.tr, new Vector2( 1, 1 ), _endColor );
+				addVert( vertIndex++, nextSegment.point, new Vector2( 1, 0.5f ), _endColor );
+				addVert( vertIndex++, nextSegment.fusedPoint, new Vector2( 1, 0 ), _endColor );
+			}
+			else
+			{
+				addVert( vertIndex++, nextSegment.fusedPoint, new Vector2( 1, 1 ), _endColor );
+				addVert( vertIndex++, nextSegment.point, new Vector2( 1, 0.5f ), _endColor );
+				addVert( vertIndex++, segment.br, new Vector2( 1, 0 ), _startColor );
+			}
+
+			addVert( vertIndex++, segment.bl, new Vector2( 0, 0 ), _startColor );
+		}
+
+
+		[MethodImpl( MethodImplOptions.AggressiveInlining )]
+		void addSegment( Segment segment, ref int vertIndex )
+		{
+			// first, we need to patch the previous elbow gap. If this is the second segment we need a different vert from the first segment
+			// since the first segment has 1 less vert than all mid segments.
+			//if( segment.hasFusedPoint )
+			{
+				if( segment.shouldFuseBottom )
+				{
+					_indices.Add( (short)vertIndex );
+					_indices.Add( (short)( vertIndex + 4 ) );
+					_indices.Add( (short)( vertIndex - 4 ) );
+				}
+				else
+				{
+					var firstSegmentOffset = vertIndex == 5 ? 1 : 0;
+					_indices.Add( (short)( vertIndex - 3 + firstSegmentOffset ) );
+					_indices.Add( (short)( vertIndex + 4 ) );
+					_indices.Add( (short)( vertIndex + 3 ) );
+				}
+			}
+			
+			_indices.Add( (short)vertIndex );
+			_indices.Add( (short)( vertIndex + 1 ) );
+			_indices.Add( (short)( vertIndex + 2 ) );
+
+			_indices.Add( (short)( vertIndex + 4 ) );
+			_indices.Add( (short)vertIndex );
+			_indices.Add( (short)( vertIndex + 2 ) );
+
+			_indices.Add( (short)( vertIndex + 3 ) );
+			_indices.Add( (short)( vertIndex + 4 ) );
+			_indices.Add( (short)( vertIndex + 2 ) );
+
+			if( segment.shouldFuseBottom )
+			{
+				addVert( vertIndex++, segment.tl, new Vector2( 0, 1 ), _startColor );
+				addVert( vertIndex++, segment.tr, new Vector2( 1, 1 ), _endColor );
+				addVert( vertIndex++, segment.br, new Vector2( 1, 0 ), _startColor );
+				addVert( vertIndex++, segment.hasFusedPoint ? segment.fusedPoint : segment.bl, new Vector2( 0, 0 ), _startColor );
+			}
+			else
+			{
+				addVert( vertIndex++, segment.hasFusedPoint ? segment.fusedPoint : segment.tl, new Vector2( 0, 1 ), _startColor );
+				addVert( vertIndex++, segment.tr, new Vector2( 1, 1 ), _endColor );
+				addVert( vertIndex++, segment.br, new Vector2( 1, 0 ), _startColor );
+				addVert( vertIndex++, segment.bl, new Vector2( 0, 0 ), _startColor );
+			}
+
+			addVert( vertIndex++, segment.point, new Vector2( 1, 0.5f ), _startColor );
+		}
+
+
+		[MethodImpl( MethodImplOptions.AggressiveInlining )]
+		void addVert( int index, Vector2 position, Vector2 texCoord, Color col )
+		{
+			if( position == Vector2.Zero )
+				Debug.log( "fuck you" );
+			
+			_vertices[index].Position = position.toVector3();
+			_vertices[index].TextureCoordinate = texCoord;
+			_vertices[index].Color = col;
 		}
 
 
@@ -300,13 +410,18 @@ namespace Nez
 
 		class Segment
 		{
-			public Vector3 tl, tr, br, bl;
-			public Vector3 point;
+			public Vector2 tl, tr, br, bl;
+			public Vector2 point;
+			public Vector2 nextPoint;
+			public Vector2 fusedPoint;
+			public bool hasFusedPoint;
+			public bool shouldFuseBottom;
 
 
-			public void setPoints( Vector3 point, Vector3 nextPoint, float firstPointWidth, float secondPointWidth )
+			public void setPoints( Vector2 point, Vector2 nextPoint, float firstPointWidth, float secondPointWidth )
 			{
 				this.point = point;
+				this.nextPoint = nextPoint;
 
 				// rotate 90 degrees before calculating and cache cos/sin
 				var radians = Mathf.atan2( nextPoint.Y - point.Y, nextPoint.X - point.X );
@@ -314,16 +429,21 @@ namespace Nez
 				var halfCos = Mathf.cos( radians ) * 0.5f;
 				var halfSin = Mathf.sin( radians ) * 0.5f;
 
-				tl = point - new Vector3( firstPointWidth * halfCos, firstPointWidth * halfSin, 0 );
-				tr = nextPoint - new Vector3( secondPointWidth * halfCos, secondPointWidth * halfSin, 0 );
-				br = nextPoint + new Vector3( secondPointWidth * halfCos, secondPointWidth * halfSin, 0 );
-				bl = point + new Vector3( firstPointWidth * halfCos, firstPointWidth * halfSin, 0 );
+				tl = point - new Vector2( firstPointWidth * halfCos, firstPointWidth * halfSin );
+				tr = nextPoint - new Vector2( secondPointWidth * halfCos, secondPointWidth * halfSin );
+				br = nextPoint + new Vector2( secondPointWidth * halfCos, secondPointWidth * halfSin );
+				bl = point + new Vector2( firstPointWidth * halfCos, firstPointWidth * halfSin );
 			}
 
 
-			public void processVerts( Segment segment )
+			public void setFusedData( bool shouldFuseBottom, Segment segment )
 			{
-				
+				this.shouldFuseBottom = shouldFuseBottom;
+
+				if( shouldFuseBottom )
+					hasFusedPoint = ShapeCollisions.lineToLine( segment.bl, segment.br, bl, br, out fusedPoint );
+				else
+					hasFusedPoint = ShapeCollisions.lineToLine( segment.tl, segment.tr, tl, tr, out fusedPoint );
 			}
 
 		}
