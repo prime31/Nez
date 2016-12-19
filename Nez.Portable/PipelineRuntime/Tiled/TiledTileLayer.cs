24b6b6305f66a7f0dde7e99ac3d512907f0bbb2a
Mike Desaro <mike@prime31.com>
2016-12-18 22:27:10 -0800

    switch tile drawing to new, faster Subtexture method

--- Nez.Portable/PipelineRuntime/Tiled/TiledTileLayer.cs
+++ Nez.Portable/PipelineRuntime/Tiled/TiledTileLayer.cs
@@ -147,7 +147,7 @@ namespace Nez.Tiled
 					if( rotation == 0 )
 						ty += ( tiledMap.tileHeight - tileRegion.sourceRect.Height );
 
-					batcher.draw( tileRegion.texture2D, new Vector2( tx, ty ), tileRegion.sourceRect, color, rotation, Vector2.Zero, 1, spriteEffects, layerDepth );
+					batcher.draw( tileRegion, new Vector2( tx, ty ), color, rotation, Vector2.Zero, 1, spriteEffects, layerDepth );
 				}
 			}
 		}
