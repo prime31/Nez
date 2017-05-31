aced8d22afa5ad7c703b76849892aa6783ce9c1f
ampetty <alexmpetty@gmail.com>
2017-05-30 21:38:35 -0700

    Split up choose functions

--- Nez.Portable/Math/Random.cs
+++ Nez.Portable/Math/Random.cs
@@ -152,11 +152,59 @@ namespace Nez
 		/// <summary>
 		/// randomly returns one of the given values
 		/// </summary>
-		/// <param name="choices">Parameter array of choices.</param>
-		/// <typeparam name="T">Value type parameter.</typeparam>
-		public static T choose<T>( params T[] choices )
+		/// <param name="first">First.</param>
+		/// <param name="second">Second.</param>
+		/// <typeparam name="T">The 1st type parameter.</typeparam>
+		public static T choose<T>( T first, T second )
 		{
-			return choices[nextInt( choices.Length )];
+			if( nextInt( 2 ) == 0 )
+				return first;
+			return second;
+		}
+
+
+		/// <summary>
+		/// randomly returns one of the given values
+		/// </summary>
+		/// <param name="first">First.</param>
+		/// <param name="second">Second.</param>
+		/// <param name="third">Third.</param>
+		/// <typeparam name="T">The 1st type parameter.</typeparam>
+		public static T choose<T>( T first, T second, T third )
+		{
+			switch( nextInt( 3 ) )
+			{
+				case 0:
+				return first;
+				case 1:
+				return second;
+				default:
+				return third;
+			}
+		}
+
+
+		/// <summary>
+		/// randomly returns one of the given values
+		/// </summary>
+		/// <param name="first">First.</param>
+		/// <param name="second">Second.</param>
+		/// <param name="third">Third.</param>
+		/// <param name="fourth">Fourth.</param>
+		/// <typeparam name="T">The 1st type parameter.</typeparam>
+		public static T choose<T>( T first, T second, T third, T fourth )
+		{
+			switch( nextInt( 4 ) )
+			{
+				case 0:
+				return first;
+				case 1:
+				return second;
+				case 2:
+				return third;
+				default:
+				return fourth;
+			}
 		}
 
 	}
