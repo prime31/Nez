65af6386690190af5ad63f320561e30d1ca16278
Nikolay Arhipov <nikolajs.arhipovs@gmail.com>
2019-08-05 02:15:35 +0300

    Updated imgui version, since the current one doesnt work on mono+linux

--- Nez.ImGui/Inspectors/SceneGraphPanes/EntityPane.cs
+++ Nez.ImGui/Inspectors/SceneGraphPanes/EntityPane.cs
@@ -87,7 +87,7 @@ namespace Nez.ImGuiTools.SceneGraphPanes
 						ImGui.CloseCurrentPopup();
 					}
 					
-					ImGui.SameLine( ImGui.GetContentRegionAvailWidth() - ImGui.GetItemRectSize().X );
+					ImGui.SameLine( ImGui.GetContentRegionAvail().X - ImGui.GetItemRectSize().X );
 
 					ImGui.PushStyleColor( ImGuiCol.Button, Microsoft.Xna.Framework.Color.Green.PackedValue );
 					if( ImGui.Button( "Create" ) )
@@ -122,7 +122,7 @@ namespace Nez.ImGuiTools.SceneGraphPanes
 						ImGui.CloseCurrentPopup();
 					}
 					
-					ImGui.SameLine( ImGui.GetContentRegionAvailWidth() - ImGui.GetItemRectSize().X );
+					ImGui.SameLine( ImGui.GetContentRegionAvail().X - ImGui.GetItemRectSize().X );
 
 					ImGui.PushStyleColor( ImGuiCol.Button, Microsoft.Xna.Framework.Color.Green.PackedValue );
 					if( ImGui.Button( "Create" ) )
