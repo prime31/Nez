6946c1205784cb660f0120acf997166900a9d629
witlock <witlock@outlook.com>
2019-10-06 16:05:18 +0300

    format

--- Nez.Portable/Assets/Tiled/Runtime/TiledRendering.cs
+++ Nez.Portable/Assets/Tiled/Runtime/TiledRendering.cs
@@ -233,7 +233,7 @@ namespace Nez.Tiled
 						pos.Y -= obj.Tile.TilesetTile.Image.Height;
 						batcher.Draw(obj.Tile.TilesetTile.Image.Texture, pos, sourceRect, Color.White, 0, Vector2.Zero, scale, spriteEffects, layerDepth);
 						goto default;
-                    case TmxObjectType.Ellipse:
+					case TmxObjectType.Ellipse:
 						pos = new Vector2(obj.X + obj.Width * 0.5f, obj.Y + obj.Height * 0.5f) * scale;
 						batcher.DrawCircle(pos, obj.Width * 0.5f, objGroup.Color);
 						goto default;
