ad0fdce74d37bf3a8b3900ddb1626647aa53606d
Nicolas Lesser <blitzrakete@gmail.com>
2019-12-22 13:16:14 +0100

    Relax GetComponent constraint.

--- Nez.Portable/ECS/Entity.cs
+++ Nez.Portable/ECS/Entity.cs
@@ -437,7 +437,7 @@ namespace Nez
 		/// </summary>
 		/// <returns>The component.</returns>
 		/// <typeparam name="T">The 1st type parameter.</typeparam>
-		public T GetComponent<T>() where T : Component => Components.GetComponent<T>(false);
+		public T GetComponent<T>() where T : class => Components.GetComponent<T>(false);
 
 		/// <summary>
 		/// Gets the first Component of type T and returns it. If no Component is found the Component will be created.
@@ -460,7 +460,7 @@ namespace Nez
 		/// <returns>The component.</returns>
 		/// <param name="onlyReturnInitializedComponents">If set to <c>true</c> only return initialized components.</param>
 		/// <typeparam name="T">The 1st type parameter.</typeparam>
-		public T GetComponent<T>(bool onlyReturnInitializedComponents) where T : Component
+		public T GetComponent<T>(bool onlyReturnInitializedComponents) where T : class
 		{
 			return Components.GetComponent<T>(onlyReturnInitializedComponents);
 		}
@@ -477,7 +477,7 @@ namespace Nez
 		/// </summary>
 		/// <returns>The component.</returns>
 		/// <typeparam name="T">The 1st type parameter.</typeparam>
-		public List<T> GetComponents<T>() where T : Component => Components.GetComponents<T>();
+		public List<T> GetComponents<T>() where T : class => Components.GetComponents<T>();
 
 		/// <summary>
 		/// removes the first Component of type T from the components list
