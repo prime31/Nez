45f1fe71b797ecef758d29976a2e4fb5648d6551
Vanit <email@lucasbrown.me>
2020-01-12 17:33:04 +1000

    Fixed reduced performance resulting from performance boost

--- Nez.Portable/Graphics/Batcher/Batcher.cs
+++ Nez.Portable/Graphics/Batcher/Batcher.cs
@@ -993,10 +993,10 @@ namespace Nez
 			#if FNA
 			fixed (VertexPositionColorTexture4* p = &_vertexInfo[0])
 			{
-				_vertexBuffer.SetDataPointerEXT(0, (IntPtr)p, _numSprites * VertexPositionColorTexture4.RealStride, SetDataOptions.Discard);
+				_vertexBuffer.SetDataPointerEXT(0, (IntPtr)p, _numSprites * VertexPositionColorTexture4.RealStride, SetDataOptions.None);
 			}
 			#else
-			_vertexBuffer.SetData(0, _vertexInfo, 0, _numSprites, VertexPositionColorTexture4.RealStride, SetDataOptions.Discard);
+			_vertexBuffer.SetData(0, _vertexInfo, 0, _numSprites, VertexPositionColorTexture4.RealStride, SetDataOptions.None);
 			#endif
 
 			curTexture = _textureInfo[0];
