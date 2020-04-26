5e61a7b1609b903bf1456e5e7e5d8c245fdce53a
toyboot4e <toyboot4e@gmail.com>
2020-04-27 01:12:29 +0900

    revert #576

--- Nez.Portable/Utils/Extensions/ComponentExt.cs
+++ Nez.Portable/Utils/Extensions/ComponentExt.cs
@@ -25,11 +25,6 @@ namespace Nez
 		{
 			return self.Entity.GetComponent<T>();
 		}
-		        
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool HasComponent<T>(this Component self) where T : Component {
-	        return self.Entity.HasComponent<T>();
-        }
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static void GetComponents<T>(this Component self, List<T> componentList) where T : class
