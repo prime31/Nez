932d533a4bea60962834e8eb2c342e5531bab5b5
bvs3 <bvansickle3@gmail.com>
2020-07-17 17:20:48 -0400

    Fixes issue with followcamera not staying centered on target when zooming

--- Nez.Portable/ECS/Components/FollowCamera.cs
+++ Nez.Portable/ECS/Components/FollowCamera.cs
@@ -87,8 +87,8 @@ namespace Nez
 		{
 			// translate the deadzone to be in world space
 			var halfScreen = Camera.Bounds.Size * 0.5f;
-			_worldSpaceDeadzone.X = Camera.Position.X - halfScreen.X + Deadzone.X + FocusOffset.X;
-			_worldSpaceDeadzone.Y = Camera.Position.Y - halfScreen.Y + Deadzone.Y + FocusOffset.Y;
+			_worldSpaceDeadzone.X = Camera.Position.X - halfScreen.X * Camera.RawZoom + Deadzone.X + FocusOffset.X;
+			_worldSpaceDeadzone.Y = Camera.Position.Y - halfScreen.Y * Camera.RawZoom + Deadzone.Y + FocusOffset.Y;
 			_worldSpaceDeadzone.Width = Deadzone.Width;
 			_worldSpaceDeadzone.Height = Deadzone.Height;
 
