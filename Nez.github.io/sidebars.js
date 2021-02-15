54dd4835f4e5ce6880b2843b27631de5abbe0ef6
janfokkeurk <janfokkeurk@gmail.com>
2021-02-15 08:03:29 +0100

    added renderers docs

--- Nez.github.io/sidebars.js
+++ Nez.github.io/sidebars.js
@@ -2,6 +2,6 @@ module.exports = {
   someSidebar: {
     'Docusaurus': ['about/introduction'],
     'Getting Started': ['getting-started/installation'],
-    'Features': ['features/core', 'features/entity-component-system'],
+    'Features': ['features/core', 'features/entity-component-system', 'features/rendering'],
   },
 };
