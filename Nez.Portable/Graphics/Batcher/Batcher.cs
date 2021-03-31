a5f14075cf8c8424539231a05b098cd0e6428249
Mike Desaro <mike@prime31.com>
2021-03-31 10:21:29 -0700

    make Batcher matrix configurable

--- Nez.Portable/Graphics/Batcher/Batcher.cs
+++ Nez.Portable/Graphics/Batcher/Batcher.cs
@@ -70,6 +70,9 @@ namespace Nez
 
 		#region static variables and constants
 
+		/// <summary>if true, the older FNA half-pixel offset will be used when creating the ortho matrix</summary>
+		public static bool UseFnaHalfPixelMatrix = false;
+
 		const int MAX_SPRITES = 2048;
 		const int MAX_VERTICES = MAX_SPRITES * 4;
 		const int MAX_INDICES = MAX_SPRITES * 6;
@@ -1052,13 +1055,16 @@ namespace Nez
 			var viewport = GraphicsDevice.Viewport;
 
 			// inlined CreateOrthographicOffCenter
-#if FNA
-			_projectionMatrix.M11 = (float)( 2.0 / (double) ( viewport.Width / 2 * 2 - 1 ) );
-			_projectionMatrix.M22 = (float)( -2.0 / (double) ( viewport.Height / 2 * 2 - 1 ) );
-#else
-			_projectionMatrix.M11 = (float) (2.0 / (double) viewport.Width);
-			_projectionMatrix.M22 = (float) (-2.0 / (double) viewport.Height);
-#endif
+			if (UseFnaHalfPixelMatrix)
+			{
+				_projectionMatrix.M11 = (float)(2.0 / (double)(viewport.Width / 2 * 2 - 1));
+				_projectionMatrix.M22 = (float)(-2.0 / (double)(viewport.Height / 2 * 2 - 1));
+			}
+			else
+			{
+				_projectionMatrix.M11 = (float)(2.0 / (double)viewport.Width);
+				_projectionMatrix.M22 = (float)(-2.0 / (double)viewport.Height);
+			}
 
 			_projectionMatrix.M41 = -1 - 0.5f * _projectionMatrix.M11;
 			_projectionMatrix.M42 = 1 - 0.5f * _projectionMatrix.M22;
