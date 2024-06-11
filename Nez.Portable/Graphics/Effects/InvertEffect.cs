0c272de7772926742c59c196d55f0987599014ff
optimo <optimo.dev@gmail.com>
2024-06-11 12:30:48 -0400

    reintroduce the InvertEffect (#812)

---
+++ Nez.Portable/Graphics/Effects/InvertEffect.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+
+namespace Nez
+{
+	public class InvertEffect : Effect
+	{
+		public InvertEffect() : base(Core.GraphicsDevice, EffectResource.InvertBytes)
+		{
+		}
+	}
+}
