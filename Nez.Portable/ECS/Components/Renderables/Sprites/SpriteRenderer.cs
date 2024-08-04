644b599bb0458ea9006231188858a2d4be875482
hershicon <hershicon@gmail.com>
2024-08-04 03:35:38 -0400

    added code to adjust local offset based on if it is flipped

--- Nez.Portable/ECS/Components/Renderables/Sprites/SpriteRenderer.cs
+++ Nez.Portable/ECS/Components/Renderables/Sprites/SpriteRenderer.cs
@@ -184,10 +184,13 @@ namespace Nez.Sprites
 			Color = originalColor;
 			_layerDepth = originalLayerDepth;
 		}
-
+		
 		public override void Render(Batcher batcher, Camera camera)
 		{
-			batcher.Draw(Sprite, Entity.Transform.Position + LocalOffset, Color,
+			Vector2 isFlippedVector = new Vector2(FlipX ? -1 : 1, FlipY ? -1 : 1);
+			Vector2 adjustedLocalOffset = LocalOffset * isFlippedVector;
+
+			batcher.Draw(Sprite, Entity.Transform.Position + adjustedLocalOffset, Color,
 				Entity.Transform.Rotation, Origin, Entity.Transform.Scale, SpriteEffects, _layerDepth);
 		}
 	}
