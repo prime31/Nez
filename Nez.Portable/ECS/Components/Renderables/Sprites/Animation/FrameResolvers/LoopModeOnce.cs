c21fa9020472c6474d86e223bc601dcdb0ffb479
Ricardo Macedo Ianelli <ricardoianelli@hotmail.com>
2024-09-08 20:42:01 -0700

    Create Loop Mode Once

---
+++ Nez.Portable/ECS/Components/Renderables/Sprites/Animation/FrameResolvers/LoopModeOnce.cs
@@ -0,0 +1,21 @@
+﻿namespace Nez.Sprites
+{
+	public class LoopModeOnce : ISpriteAnimationLoopMode
+	{
+		/// <summary>
+		/// Play the sequence once [A][B][C] then pause and set time to 0 [A]
+		/// </summary>
+		public void NextFrame(SpriteAnimator animator)
+		{
+			var newFrame = animator.CurrentFrame + 1;
+			if (newFrame >= animator.FrameCount)
+			{
+				animator.SetCompleted(true);
+			}
+			else
+			{
+				animator.SetFrame(newFrame);
+			}
+		}
+	}
+}
