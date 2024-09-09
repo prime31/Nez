8faae632313821d5c81c4fa636ab9c20ad5eabd9
Ricardo Macedo Ianelli <ricardoianelli@hotmail.com>
2024-09-08 20:42:07 -0700

    Create Loop Mode Clamp Forever

---
+++ Nez.Portable/ECS/Components/Renderables/Sprites/Animation/FrameResolvers/LoopModeClampForever.cs
@@ -0,0 +1,21 @@
+﻿namespace Nez.Sprites
+{
+	public class LoopModeClampForever : ISpriteAnimationLoopMode
+	{
+		/// <summary>
+		/// Plays back the animation once, [A][B][C]. When it reaches the end, it will keep playing the last frame and never stop playing
+		/// </summary>
+		public void NextFrame(SpriteAnimator animator)
+		{
+			var newFrame = animator.CurrentFrame + 1;
+			if (newFrame >= animator.FrameCount)
+			{
+				animator.SetCompleted();
+			}
+			else
+			{
+				animator.SetFrame(newFrame);
+			}
+		}
+	}
+}
