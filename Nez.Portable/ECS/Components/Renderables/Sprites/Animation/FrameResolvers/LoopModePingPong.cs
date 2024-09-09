c231e5cb191d1d562dfdc18540f071fd7409248a
Ricardo Macedo Ianelli <ricardoianelli@hotmail.com>
2024-09-08 20:42:13 -0700

    Create Loop Mode Ping Pong

---
+++ Nez.Portable/ECS/Components/Renderables/Sprites/Animation/FrameResolvers/LoopModePingPong.cs
@@ -0,0 +1,60 @@
+﻿namespace Nez.Sprites
+{
+	public class LoopModePingPong : ISpriteAnimationLoopMode
+	{
+		private enum State
+		{
+			Ping,
+			Pong
+		}
+
+		private State _currentState = State.Ping;
+		
+		/// <summary>
+		/// Play the sequence in a ping pong loop forever [A][B][C][B][A][B][C][B]...
+		/// </summary>
+		public void NextFrame(SpriteAnimator animator)
+		{
+			// If there's only one frame, no need to change it.
+			if (animator.FrameCount == 1) return;
+			
+			switch (_currentState)
+			{
+				case State.Ping:
+					ParsePing(animator);
+					break;
+				case State.Pong:
+					ParsePong(animator);
+					break;
+			}
+		}
+
+		private void ParsePing(SpriteAnimator animator)
+		{
+			var newFrame = animator.CurrentFrame + 1;
+			if (newFrame >= animator.FrameCount)
+			{
+				_currentState = State.Pong;
+				ParsePong(animator);
+			}
+			else
+			{
+				animator.SetFrame(newFrame);
+			}
+		}
+		
+		private void ParsePong(SpriteAnimator animator)
+		{
+			var newFrame = animator.CurrentFrame - 1;
+			if (newFrame < 0)
+			{
+				_currentState = State.Ping;
+				ParsePing(animator);
+			}
+			else
+			{
+				animator.SetFrame(newFrame);
+			}
+		}
+	}
+}
