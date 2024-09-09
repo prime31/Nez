61436efeb81b5eeac9111d6208207873cdae53e0
Ricardo Macedo Ianelli <ricardoianelli@hotmail.com>
2024-09-08 20:41:54 -0700

    Create LoopMode Loop

---
+++ Nez.Portable/ECS/Components/Renderables/Sprites/Animation/FrameResolvers/LoopModeLoop.cs
@@ -0,0 +1,13 @@
+﻿namespace Nez.Sprites
+{
+	public class LoopModeLoop : ISpriteAnimationLoopMode
+	{
+		/// <summary>
+		/// Play the sequence in a loop forever [A][B][C][A][B][C][A][B][C]...
+		/// </summary>
+		public void NextFrame(SpriteAnimator animator)
+		{
+			animator.SetFrame((animator.CurrentFrame + 1) % animator.FrameCount);
+		}
+	}
+}
