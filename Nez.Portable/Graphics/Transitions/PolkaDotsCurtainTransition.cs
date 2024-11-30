0b3d2c31c771d4fd5868e391543dab67a48d83c8
optimo <optimo.dev@gmail.com>
2024-11-30 12:53:34 -0500

    first batch converted from gl-transitions

---
+++ Nez.Portable/Graphics/Transitions/PolkaDotsCurtainTransition.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using Nez.Tweens;
+
+namespace Nez
+{
+	/// <summary>
+	/// Grid of dots that grow. No aspect ratio correction
+	/// based on: https://gl-transitions.com/editor/PolkaDotsCurtain
+	/// </summary>
+	public class PolkaDotsCurtainTransition : SceneTransition
+	{
+		/// <summary>
+		/// Number of dots per axis. Defaults to 20.
+		/// </summary>
+		/// <value>The number of dots per axis.</value>
+		public float Dots
+		{
+			set => _effect.Parameters["_dots"].SetValue(value);
+		}
+
+		/// <summary>
+		/// Origin of dots animating. Defaults to Vector2(0, 0). "Top-Left"
+		/// </summary>
+		/// <value>The center origin.</value>
+		public Vector2 Center
+		{
+			set => _effect.Parameters["_center"].SetValue(value);
+		}
+
+		/// <summary>
+		/// duration for the transition
+		/// </summary>
+		public float Duration = 1f;
+
+		/// <summary>
+		/// ease equation to use for the animation
+		/// </summary>
+		public EaseType EaseType = EaseType.Linear;
+
+		Effect _effect;
+		Rectangle _destinationRect;
+
+		public PolkaDotsCurtainTransition(Func<Scene> sceneLoadAction) : base(sceneLoadAction, true)
+		{
+			_destinationRect = PreviousSceneRender.Bounds;
+
+			// load Effect and set defaults
+			_effect = Core.Content.LoadEffect("Content/nez/effects/transitions/PolkaDotsCurtain.mgfxo");
+			Center = new Vector2(0, 0);
+			Dots = 20f;
+		}
+
+		public override IEnumerator OnBeginTransition()
+		{
+			yield return null;
+
+			// load up the new Scene
+			yield return Core.StartCoroutine(LoadNextScene());
+
+			// apply the effect
+			yield return Core.StartCoroutine(TickEffectProgressProperty(_effect, Duration, EaseType));
+
+			TransitionComplete();
+
+			// cleanup
+			Core.Content.UnloadEffect(_effect);
+		}
+
+		public override void Render(Batcher batcher)
+		{
+			Core.GraphicsDevice.SetRenderTarget(null);
+			batcher.Begin(BlendState.NonPremultiplied, Core.DefaultSamplerState, DepthStencilState.None, null, _effect);
+			batcher.Draw(PreviousSceneRender, _destinationRect, Color.White);
+			batcher.End();
+		}
+	}
+}
---
+++ Nez.Portable/Graphics/Transitions/SimpleZoomTransition.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using Nez.Tweens;
+
+namespace Nez
+{
+	/// <summary>
+	/// Zoom in while blurring
+	/// based on: https://gl-transitions.com/editor/SimpleZoom
+	/// </summary>
+	public class SimpleZoomTransition : SceneTransition
+	{
+		/// <summary>
+		/// Speed of the zoom. Defaults to 0.8. (0.2 - 1.0)
+		/// </summary>
+		/// <value>The zoom quickness.</value>
+		public float ZoomQuickness
+		{
+			set => _effect.Parameters["_zoomQuickness"].SetValue(Mathf.Clamp(value, 0.2f, 1.0f));
+		}
+
+		/// <summary>
+		/// duration for the transition
+		/// </summary>
+		public float Duration = 1f;
+
+		/// <summary>
+		/// ease equation to use for the animation
+		/// </summary>
+		public EaseType EaseType = EaseType.Linear;
+
+		Effect _effect;
+		Rectangle _destinationRect;
+
+		public SimpleZoomTransition(Func<Scene> sceneLoadAction) : base(sceneLoadAction, true)
+		{
+			_destinationRect = PreviousSceneRender.Bounds;
+
+			// load Effect and set defaults
+			_effect = Core.Content.LoadEffect("Content/nez/effects/transitions/SimpleZoom.mgfxo");
+			ZoomQuickness = 0.8f;
+		}
+
+		public override IEnumerator OnBeginTransition()
+		{
+			yield return null;
+
+			// load up the new Scene
+			yield return Core.StartCoroutine(LoadNextScene());
+
+			// apply the effect
+			yield return Core.StartCoroutine(TickEffectProgressProperty(_effect, Duration, EaseType));
+
+			TransitionComplete();
+
+			// cleanup
+			Core.Content.UnloadEffect(_effect);
+		}
+
+		public override void Render(Batcher batcher)
+		{
+			Core.GraphicsDevice.SetRenderTarget(null);
+			batcher.Begin(BlendState.NonPremultiplied, Core.DefaultSamplerState, DepthStencilState.None, null, _effect);
+			batcher.Draw(PreviousSceneRender, _destinationRect, Color.White);
+			batcher.End();
+		}
+	}
+}
