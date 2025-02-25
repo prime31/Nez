67bef24787943a338297ae8bb7fd39d4409c6907
optimo <optimo.dev@gmail.com>
2025-02-24 20:09:27 -0500

    fix spaces -> tabs

--- Nez.ImGui/Inspectors/ObjectInspectors/SceneComponentInspector.cs
+++ Nez.ImGui/Inspectors/ObjectInspectors/SceneComponentInspector.cs
@@ -5,50 +5,50 @@ using Nez.ImGuiTools.TypeInspectors;
 
 namespace Nez.ImGuiTools.ObjectInspectors
 {
-    public class SceneComponentInspector
-    {
-        public SceneComponent SceneComponent => _sceneComponent;
-        protected List<AbstractTypeInspector> _inspectors;
+	public class SceneComponentInspector
+	{
+		public SceneComponent SceneComponent => _sceneComponent;
+		protected List<AbstractTypeInspector> _inspectors;
 
-        protected int _scopeId = NezImGui.GetScopeId();
-        string _name;
-        SceneComponent _sceneComponent;
+		protected int _scopeId = NezImGui.GetScopeId();
+		string _name;
+		SceneComponent _sceneComponent;
 
-        public SceneComponentInspector(SceneComponent sceneComponent)
-        {
-            _sceneComponent = sceneComponent;
-            _inspectors = TypeInspectorUtils.GetInspectableProperties(sceneComponent);
-            _name = _sceneComponent.GetType().Name;
-        }
+		public SceneComponentInspector(SceneComponent sceneComponent)
+		{
+			_sceneComponent = sceneComponent;
+			_inspectors = TypeInspectorUtils.GetInspectableProperties(sceneComponent);
+			_name = _sceneComponent.GetType().Name;
+		}
 
-        public void Draw()
-        {
-            ImGui.PushID(_scopeId);
-            var isOpen = ImGui.CollapsingHeader(_name);
+		public void Draw()
+		{
+			ImGui.PushID(_scopeId);
+			var isOpen = ImGui.CollapsingHeader(_name);
 
-            NezImGui.ShowContextMenuTooltip();
+			NezImGui.ShowContextMenuTooltip();
 
-            if (ImGui.BeginPopupContextItem())
-            {
-                if (ImGui.Selectable("Remove SceneComponent"))
-                {
-                    isOpen = false;
-                    Core.Scene.RemoveSceneComponent(_sceneComponent);
-                    ImGui.CloseCurrentPopup();
-                }
+			if (ImGui.BeginPopupContextItem())
+			{
+				if (ImGui.Selectable("Remove SceneComponent"))
+				{
+					isOpen = false;
+					Core.Scene.RemoveSceneComponent(_sceneComponent);
+					ImGui.CloseCurrentPopup();
+				}
 
-                ImGui.EndPopup();
-            }
+				ImGui.EndPopup();
+			}
 
-            if (isOpen)
-            {
-                ImGui.Indent();
-                foreach (var inspector in _inspectors)
-                    inspector.Draw();
-                ImGui.Unindent();
-            }
+			if (isOpen)
+			{
+				ImGui.Indent();
+				foreach (var inspector in _inspectors)
+					inspector.Draw();
+				ImGui.Unindent();
+			}
 
-            ImGui.PopID();
-        }
-    }
+			ImGui.PopID();
+		}
+	}
 }
