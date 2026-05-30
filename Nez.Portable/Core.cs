1e3d3dd0cd26aeb9f66c02d71375dfcc41ef4a2e
George Allen <george.allen93@outlook.com>
2026-05-30 14:32:34 +0100

    Bump to new monogame

--- Nez.Portable/Core.cs
+++ Nez.Portable/Core.cs
@@ -358,7 +358,7 @@ namespace Nez
 			EndDebugDraw();
 		}
 
-		protected override void OnExiting(object sender, EventArgs args)
+		protected override void OnExiting(object sender, ExitingEventArgs args)
 		{
 			base.OnExiting(sender, args);
 			Emitter.Emit(CoreEvents.Exiting);
